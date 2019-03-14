e8998da976529c3d8f82f5b5966d94248bf1865d
Ward Louckx <ward@louckx.com>
2019-03-14 22:15:12 +0100

    clear screen written

--- PongCore/Program.cs
+++ PongCore/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace PongCore
 {
@@ -6,7 +7,28 @@ namespace PongCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Get current console config
+            int origScreenSizeX = WindowWidth;
+            int origScreenSizeY = WindowHeight;
+            ConsoleColor origForegroundColor = ForegroundColor;
+            ConsoleColor origBackgroundColor = BackgroundColor;
+            bool origCursorVisible = CursorVisible;
+            int origCursorSize = CursorSize;
+            int origBufferWidth = BufferWidth;
+            int origBufferHeight = BufferHeight;
+
+            // Clear the screen
+            Clear();
+            
+            // set up our colors, window size to the old DOS format 80 x 25
+            // if not allowed return with error and wait for user to quit
+            SetCursorPosition(0, origScreenSizeY/2);
+            CursorVisible = true;
+            CursorSize = 100;
+            Write("Setting up the screen");
+            System.Threading.Thread.Sleep(500);
+            Read();
+            
         }
     }
 }

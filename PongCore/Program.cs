706f2b98ac816cad8345de3ab38f26494ca72cd9
Ward Louckx <ward@louckx.com>
2019-03-15 21:44:29 +0100

    Added "Forward to the past"

--- PongCore/Program.cs
+++ PongCore/Program.cs
@@ -22,11 +22,38 @@ namespace PongCore
             
             // set up our colors, window size to the old DOS format 80 x 25
             // if not allowed return with error and wait for user to quit
-            SetCursorPosition(0, origScreenSizeY/2);
+            SetCursorPosition(0, WindowHeight/2);
             CursorVisible = true;
             CursorSize = 100;
-            Write("Setting up the screen");
-            System.Threading.Thread.Sleep(500);
+            Write("Traveling back in time to the 80\'s");
+            System.Threading.Thread.Sleep(1500);
+
+            SetWindowSize(1, 1);
+            SetBufferSize(80, 25);
+            SetWindowSize(80, 25);
+            // flash colors a bit for CRT feeling :)
+            int i = 0;
+            while (i<4)
+            {
+                if (BackgroundColor == ConsoleColor.White)
+                {
+                    BackgroundColor = ConsoleColor.Black;
+                }
+                else
+                {
+                    BackgroundColor = ConsoleColor.White;
+                }
+                if (i == 1)
+                {
+                    ForegroundColor = ConsoleColor.Green;
+                }
+                Clear();
+                SetCursorPosition(0, WindowHeight/ 2);
+                Write("Traveling back in time to the 80\'s");
+                System.Threading.Thread.Sleep(100);
+                i++;
+            }
+            
             Read();
             
         }

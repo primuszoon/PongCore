fb9a244e9be53caa45382a47976fb5cb64cd49dd
Ward Louckx <ward@louckx.com>
2019-03-17 16:10:00 +0100

    Added drawcall method

--- PongCore/Program.cs
+++ PongCore/Program.cs
@@ -98,7 +98,127 @@ namespace PongCore
             SetCursorPosition(0, WindowHeight-1);
             Write("Press Any Key to Continue... ");
             Read();
+            Clear();
             
+            string playerPad = "--";
+            // Program loop for testing
+            do
+            {
+                while (!KeyAvailable)
+                {
+                    DrawField(1, 5, 8, 15, 14, 20);
+                }
+            } while (ReadKey(true).Key != ConsoleKey.Escape);
+
+          
+            
+            
+        }
+
+        static void DrawField(int scorePC,int scoreP1,int pcX,int p1X,int ballX,int ballY)
+        {
+            /* draw structure
+             *  
+             * Draw whole area, draw per line
+             * 
+             * Calculate position of pads -> done by main loop
+             * Calculate position of ball -> done by main loop
+             * 
+             * First line contains score (invert BG and FG color)
+             * 
+             * Draw PC pad line
+             * Empty lines where there is no ball
+             * Draw ball line
+             * empty lines where there is no ball
+             * Draw Player pad line
+             * 
+             * 
+             */
+
+            // get current screen size
+            int screenX = WindowWidth;
+            int screenY = WindowHeight-1;
+
+            // cursor visibility false in drawcall to avoid effects of resizing
+            CursorVisible = false;
+
+            // create string array as big as screenY for looping through it
+            string[] lines = new string[screenY];
+
+            // elements
+            string sPC = $"PC: {scorePC.ToString()}";
+            string sP1 = $"Player: {scoreP1.ToString()}";
+            string title = "PONG";
+            string ball = "0";
+            string pad = "--";
+            int i;
+            int j;
+            string spaceI;
+            string spaceJ;
+         
+            // score line
+            i = (screenX / 2) - sPC.Length - (title.Length / 2);
+            j = (screenX / 2) - sP1.Length - (title.Length / 2);
+            spaceI = new string(' ', i);
+            spaceJ = new string(' ', j);
+            string[] titleLine = {sPC,spaceI,title,spaceJ,sP1 };
+            lines[0] = string.Concat(titleLine);
+
+            // Computer Pad line
+            i = pcX;
+            j = screenX - pad.Length - i;
+            spaceI = new string(' ', i);
+            spaceJ = new string(' ', j);
+            string[] pcPadLine = { spaceI, pad, spaceJ };
+            lines[1] = string.Concat(pcPadLine);
+
+            // Ball line
+            i = ballX;
+            j = screenX - ball.Length - i;
+            spaceI = new string(' ', i);
+            spaceJ = new string(' ', j);
+            string[] ballLine = { spaceI, ball, spaceJ };
+            lines[ballY] = string.Concat(ballLine);
+
+            // Player Pad line
+            i = p1X;
+            j = screenX - pad.Length - i;
+            spaceI = new string(' ', i);
+            spaceJ = new string(' ', j);
+            string[] p1PadLine = { spaceI, pad, spaceJ };
+            lines[lines.GetUpperBound(0)] = string.Concat(p1PadLine);
+
+            // fill the array with empty lines where needed
+            spaceI = new string(' ', screenX);
+            for(int k=2;k<lines.GetUpperBound(0); k++)
+            {
+                if (k != ballY)
+                {
+                    lines[k] = spaceI;
+                }
+            }
+
+            // Start drawing
+            for(int a=0;a<=lines.GetUpperBound(0);a++)
+            {
+                SetCursorPosition(0, a);
+                if (a == 0)
+                {
+                    ForegroundColor = ConsoleColor.Black;
+                    BackgroundColor = ConsoleColor.Green;
+                    Write(lines[a]);
+                    ForegroundColor = ConsoleColor.Green;
+                    BackgroundColor = ConsoleColor.Black;
+                }
+                else
+                {
+                    Write(lines[a]);
+                }
+            }
+
+            // reduce flickering
+            System.Threading.Thread.Sleep(50);
+
         }
     }
 }
